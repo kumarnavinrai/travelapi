7e7fd91104cc6b056f97d056376e9bdb888c58ef
Navin <navin@example.com>
2016-10-17 16:22:53 +0530

    17-10-2016 chnages

--- themes/bartik/templates/footer.php
+++ themes/bartik/templates/footer.php
@@ -50,7 +50,7 @@ trk=nav_responsive_tab_profile"></a>
                     <div class="col-md-4">
                         <h4>Have Questions?</h4>
                         <h4 class="text-color">1-888-417-0446</h4>
-                        <h4><a href="#" class="text-color">support@travelpainters.com</a></h4>
+                        <h4><a href="#" class="text-color">support@flyoticket.com</a></h4>
                         <p>24/7 Dedicated Customer Support</p>
                     </div>
 
--- themes/bartik/templates/html--homepage.tpl.php
+++ themes/bartik/templates/html--homepage.tpl.php
@@ -101,7 +101,7 @@ function get_client_ip() {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>"  ng-app="myApp" id="filghtCtrlHomeId" ng-controller="filghtCtrlHome"  version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>" <?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
-  <title>Travel Painters</title>
+  <title>Flyoticket</title>
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta name="keywords" content="Flyoticket,Travel Painters,Car,hotel,flight,booking flight" />
     <meta name="description" content="Flyoticket - A Premium Booking for travel">
