dc15e2d1073f27ad10b88c93cf444f964609bc79
Navin <navin@example.com>
2016-11-25 14:33:32 +0530

    popular routes

---
+++ themes/bartik/templates/common/cclick.php
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+	var script = document.createElement("script");
+	script.async = true; script.type = "text/javascript";
+	var target = 'https://clickcease.com/monitor/stat.js';
+	script.src = target;
+	var elem = document.head;
+	elem.appendChild(script);
+</script>
+<noscript><a href="https://clickcease.com"><img src="https://monitor.clickcease.com/stats/stats.aspx" alt="Click Fraud Protection"/></a></noscript>
